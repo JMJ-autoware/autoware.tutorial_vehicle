2a751f1efa873e7bace6ef25fc8712876a62abeb
jmj073 <wjsalswns0733@gmail.com>
2024-06-20 17:39:17 +0900

    add build.sh

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+exec 3>&1
+
+build_target() {
+  local regex='Failed *<* *(\w+)'
+  local target=$1
+  local options=( "${@:2}" )
+
+  while true; do
+    if [ -n "$target" ]; then
+      echo "<| build $target |>"
+      local err=$(colcon build --packages-up-to "$target" "${options[@]}" 2>&1 1>&3 | tee /dev/stderr)
+    else
+      echo '<| build all |>'
+      local err=$(colcon build "${options[@]}" 2>&1 1>&3 | tee /dev/stderr)
+    fi
+
+    if [[ $err =~ $regex ]]; then
+      build_target "${BASH_REMATCH[1]}" "${options[@]}"
+    else
+      break
+    fi
+  done
+
+}
+
+main() {
+  if [[ $1 == -* ]]; then
+    build_target "" "$@"
+  else
+    build_target "$@"
+  fi
+}
+
+main "$@"
